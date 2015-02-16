ca34114a456d13dc26c3b83e66c35c321ece86c6
Shawn Aukstakalnis <Shawn.Aukstakalnis@gmail.com>
2015-02-16 18:46:02 -0500

    replaces tabbed indents with spaces

--- app/controllers/api/v1/journal_categories_controller.rb
+++ app/controllers/api/v1/journal_categories_controller.rb
@@ -1,22 +1,22 @@
 module Api
-	module V1
-	
-		class JournalCategoriesController < ApiController
+  module V1
+  
+    class JournalCategoriesController < ApiController
 
-			before_filter :require_oauth_application	
-			respond_to    :json
+      before_filter :require_oauth_application  
+      respond_to    :json
 
-		  def index
-		  	response = { journal_categories: JournalCategory.all }
-		  	render json: response
-		  end
+      def index
+        response = { journal_categories: JournalCategory.all }
+        render json: response
+      end
 
-		  def core
-		  	response = { journal_categories: JournalCategory.where(core:true) }
-		  	render json: response
-		  end
+      def core
+        response = { journal_categories: JournalCategory.where(core:true) }
+        render json: response
+      end
 
-	 	end
+    end
 
-	end
+  end
 end
--- app/controllers/api/v1/moods_controller.rb
+++ app/controllers/api/v1/moods_controller.rb
@@ -1,54 +1,54 @@
 module Api
-	module V1
+  module V1
 
-		class MoodsController < ApiController
+    class MoodsController < ApiController
 
-			before_filter :require_oauth_application,:doorkeeper_authorize!
-			respond_to :json
+      before_filter :require_oauth_application,:doorkeeper_authorize!
+      respond_to :json
 
-			def create
+      def create
 
-				mood = current_resource_owner.moods.new(mood_params(params[:mood]))
+        mood = current_resource_owner.moods.new(mood_params(params[:mood]))
 
-				if mood.valid?
-					mood.save
-					render json: { mood: mood }
-				else
-					render json: { errors: mood.errors }, status: 422
-				end	
+        if mood.valid?
+          mood.save
+          render json: { mood: mood }
+        else
+          render json: { errors: mood.errors }, status: 422
+        end 
 
-			end
+      end
 
-			def update
-				mood = current_resource_owner.moods.find(params[:id])
-				ap params[:mood]
-				if mood.update(mood_params(params[:mood]))
-					render json: { mood: mood}
-				else
-					render json: { errors: mood.errors, status: 422}
-				end
-			end
+      def update
+        mood = current_resource_owner.moods.find(params[:id])
+        ap params[:mood]
+        if mood.update(mood_params(params[:mood]))
+          render json: { mood: mood}
+        else
+          render json: { errors: mood.errors, status: 422}
+        end
+      end
 
-			def destroy
-				current_resource_owner.moods.find(params[:id]).destroy
-				head 204
-			end
+      def destroy
+        current_resource_owner.moods.find(params[:id]).destroy
+        head 204
+      end
 
-			def show
-				render json: current_resource_owner.moods.find(params[:id])
-			end
+      def show
+        render json: current_resource_owner.moods.find(params[:id])
+      end
 
-			protected
+      protected
 
-			def mood_params(hash)
-				params = ActionController::Parameters.new(hash)
-				params.permit(
-					:created_at,
-					:score
-				)
-			end
+      def mood_params(hash)
+        params = ActionController::Parameters.new(hash)
+        params.permit(
+          :created_at,
+          :score
+        )
+      end
 
-		end
-	
-	end
+    end
+  
+  end
 end
