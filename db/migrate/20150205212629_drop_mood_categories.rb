1c81dcff78f90a26c0de458a67df8449215bef65
mikekavouras <kavourasm@gmail.com>
2015-04-28 17:10:14 -0400

    remove conflicting migration line

--- db/migrate/20150205212629_drop_mood_categories.rb
+++ db/migrate/20150205212629_drop_mood_categories.rb
@@ -4,7 +4,6 @@ class DropMoodCategories < ActiveRecord::Migration
     remove_column :user_mood_categories, :user_id, :integer
     remove_column :user_mood_categories, :mood_category_id, :integer
 
-    drop_table :moods
     drop_table :mood_categories
   	drop_table :user_mood_categories
   end
