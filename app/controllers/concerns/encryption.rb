ccf20b3c84c8be90d1187f836b4078a85319abf2
Shawn Aukstakalnis <Shawn.Aukstakalnis@gmail.com>
2015-04-29 19:47:37 -0400

    fixes encryption data key name

--- app/controllers/concerns/encryption.rb
+++ app/controllers/concerns/encryption.rb
@@ -6,7 +6,7 @@ module Encryption
   def data_encryption_key
     # if in production. require key to be set.
     if Rails.env.production?
-      raise 'Must set data encryption key!!' unless ENV['TOKEN_KEY']
+      raise 'Must set data encryption key!!' unless ENV['DATA_KEY']
       ENV['DATA_KEY']
     else
       ENV['DATA_KEY'] ? ENV['DATA_KEY'] : 'super_secret_test_key'
