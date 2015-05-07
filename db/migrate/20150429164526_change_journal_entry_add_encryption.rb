7a15172142f35044dc75cb3522634c5b9c6493b2
Shawn Aukstakalnis <Shawn.Aukstakalnis@gmail.com>
2015-05-06 20:29:42 -0400

    fixes migration

--- db/migrate/20150429164526_change_journal_entry_add_encryption.rb
+++ db/migrate/20150429164526_change_journal_entry_add_encryption.rb
@@ -1,7 +1,7 @@
 class ChangeJournalEntryAddEncryption < ActiveRecord::Migration
   def change
     rename_column :journal_entries, :note, :encrypted_note
-    #add_column :journal_entries, :encrypted_note_salt, :string
-    #add_column :journal_entries, :encrpyted_note_iv, :string  
+    add_column :journal_entries, :encrypted_note_salt, :string
+    add_column :journal_entries, :encrpyted_note_iv, :string  
   end
 end
