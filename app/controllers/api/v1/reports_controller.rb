5b1ead0f256e09a9095616498d21eb69dadb9377
Shawn Aukstakalnis <Shawn.Aukstakalnis@gmail.com>
2015-05-08 17:05:32 -0400

    Bases day on timezone_offset

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -5,11 +5,15 @@ module Api
       respond_to :json
 
       def average_score_by_date
-        je_report_query = current_resource_owner.journal_entries.joins(:journal_categories)
-        je_report_query = je_report_query.where("timestamp >= ?", params[:start_date]) unless params[:start_date].blank?
-        je_report_query = je_report_query.where("timestamp <= ?", params[:end_date]) unless params[:end_date].blank?
+
+        s = DateTime.parse(params[:start_date].to_date.to_s) + params[:timezone_offset].to_i.hours
+        e = DateTime.parse(params[:end_date].to_date.to_s) + params[:timezone_offset].to_i.hours
+
+        current_resource_owner.journal_entries.eager_load(:journal_categories)
+        je_report_query = current_resource_owner.journal_entries.where("timestamp >= ?", s) unless params[:start_date].blank?
+        je_report_query = je_report_query.where("timestamp <= ?", e) unless params[:end_date].blank?
         je_report_query = je_report_query.where("journal_categories.name = ?", params[:category]) unless params[:category].blank?
-        result = je_report_query.group("date(timestamp)").order("date_timestamp desc").average(:score)
+        result = je_report_query.group("date(timestamp + '#{params[:timezone_offset].to_i} hours'::interval )").average(:score)
 
         #Maps average from BigDecimal to float since BigDecimal is serialized as a string when converted to json.
         render json: result.map { |k, avg| { k => avg.to_f } } 
